14cf6d10ffa57104c3dd38dee81798a00053913d
masa <masa016@gmail.com>
2021-01-22 23:05:31 +0900

    no message

--- drh_dev/wsgi.py
+++ drh_dev/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drh_dev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drh_dev.settings.production')
 
 application = get_wsgi_application()
